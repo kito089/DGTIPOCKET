80d2b5943106f1308ae9b28c3d3d2c6422973e80
kito089 <21301061550046@cetis155.edu.mx>
2023-10-23 11:01:19 -0600

    cambio de redireccion al no tener una sesion uwu

--- pagina_android/python/funciones_auth.py
+++ pagina_android/python/funciones_auth.py
@@ -1,4 +1,4 @@
-from flask import session
+from flask import session, redirect, url_for
 from functools import wraps
 
 
@@ -10,5 +10,5 @@ def login_required(f):
         # the other data for that user/check if they exist
         if user:
             return f(*args, **kwargs)
-        return 'You aint logged in, no page for u!'
+        return redirect(url_for("login"))
     return decorated_function

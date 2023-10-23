7d487b6dc846a930f1a9b454cec3e809f82a59c1
kito089 <21301061550046@cetis155.edu.mx>
2023-10-23 11:12:22 -0600

    se supone q deberia de estar como antes :v

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -25,12 +25,12 @@ google = oauth.register(
     name='google',
     client_id= os.getenv("GOOGLE_CLIENT_ID"),
     client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
+    #access_token_url='https://accounts.google.com/o/oauth2/token',
+    #access_token_params=None,
+    #authorize_url='https://accounts.google.com/o/oauth2/auth',
+    #authorize_params=None,
     api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    #userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
     #--server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
 )
@@ -41,7 +41,6 @@ def index():
     bd = Coneccion()
     avisos = bd.obtenerTablas("noticias")
     concursos = bd.obtenerTablas("noticias")
-
     bd.exit()
     if 'google_token' in session:
         resp = oauth.google.get('userinfo')

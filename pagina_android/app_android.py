00cad9c318e5b0c2844342d7f07df5f9707f96b0
kito089 <21301061550046@cetis155.edu.mx>
2023-10-25 11:45:25 -0600

    sera?

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 # XDDDDDDDD
 # Session config
 app.secret_key = os.getenv("APP_SECRET_KEY")
-app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+#app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+#app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
 oauth = OAuth(app)
 
@@ -25,12 +25,12 @@ google = oauth.register(
     name='google',
     client_id= '1092101831178-qmrflc090f71mb558vd9865cp70sfgpf.apps.googleusercontent.com',
     client_secret='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',
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
+    #access_token_url='https://accounts.google.com/o/oauth2/token',
+    #access_token_params=None,
+    #authorize_url='https://accounts.google.com/o/oauth2/auth',
+    #authorize_params=None,
     api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
+    #userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
     #--server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
 )

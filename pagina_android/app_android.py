10299a28fa127b29e1746d82342cc1308592bcd3
kito089 <21301061550046@cetis155.edu.mx>
2023-11-07 10:25:46 -0600

    prueba antes de q empieze a fallar unu

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -12,20 +12,20 @@ app = Flask(__name__)
 
 # XDDDDDDDD
 # Session config
-app.secret_key = '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'
-##app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
-##app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
+app.secret_key = os.getenv("APP_SECRET_KEY")
+app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
 oauth = OAuth(app)
 
 google = oauth.register(
     name='google',
-    client_id='1092101831178-qmrflc090f71mb558vd9865cp70sfgpf.apps.googleusercontent.com',
-    client_secret='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',
-    #access_token_url='https://accounts.google.com/o/oauth2/token',
-    #access_token_params=None,
-    #authorize_url='https://accounts.google.com/o/oauth2/auth',
-    #authorize_params=None,
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    access_token_params=None,
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+    authorize_params=None,
     api_base_url='https://www.googleapis.com/oauth2/v1/',
     #--userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',

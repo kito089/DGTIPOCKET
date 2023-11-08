c8313462ca1cdbe5aaf9c7a56f74ad41a7030ade
kito089 <21301061550046@cetis155.edu.mx>
2023-11-08 11:27:58 -0600

    para mas rapido :D

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -21,9 +21,6 @@ app.secret_key = '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'
 #app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
 #app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
-print("\n\n\n\n\ntengo fe: \n\n\n\n\n")
-#print(os.getenv("APP_SECRET_KEY"))
-
 oauth = OAuth(app)
 
 google = oauth.register(
@@ -43,6 +40,8 @@ google = oauth.register(
 # Definir una ruta para la página principal
 @app.route('/')
 def index():
+    print("\n\n\n\n\ntengo fe: \n\n\n\n\n")
+    #print(os.getenv("APP_SECRET_KEY"))
     return render_template('indexapp.html')
 
 @app.route("/prueba")

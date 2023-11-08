fb9625b285cbaaade533c657788d8e2be2c93567
kito089 <21301061550046@cetis155.edu.mx>
2023-11-08 12:04:31 -0600

    animoooooooooooo

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -5,7 +5,7 @@ from python.bd import *
 from datetime import timedelta
 
 # decorator for routes that should be accessible only by logged in users
-from auth_decorator import login_required
+from funciones_auth import login_required
 
 # dotenv setup
 from dotenv import load_dotenv
@@ -46,10 +46,8 @@ def index():
 @app.route("/prueba")
 def prueba():
     print("aki: ")
-    parametros = session.get('profile', {})
-    print(parametros)
-    print(parametros['email'])
-    return 'Hola ' + parametros['name']
+    email = dict(session)['profile']['email']
+    return 'Hola ' + email
 
 @app.route('/sesion')
 def sesion():
@@ -70,7 +68,7 @@ def logout():
 def authorize():
     google = oauth.create_client('google')  # create the google oauth client
     token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    resp = google.get('userinfo')   # userinfo contains stuff u specificed in the scrope
+    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
     user_info = resp.json()
     user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
     # Here you use the profile/user data that you got and query your database find/register the user
--- pagina_android/python/funciones_auth.py
+++ pagina_android/python/funciones_auth.py
@@ -6,7 +6,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         print("----------entre?")
-        user = session.get('profile', {})
+        user = dict(session).get('profile', None)
         # You would add a check here and usethe user id or something to fetch
         # the other data for that user/check if they exist
         if user:

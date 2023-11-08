47d7a93ef47c4334995fd274efd2207c51e8cd24
kito089 <21301061550046@cetis155.edu.mx>
2023-11-08 11:55:32 -0600

    prints :)

--- pagina_android/python/funciones_auth.py
+++ pagina_android/python/funciones_auth.py
@@ -5,10 +5,13 @@ from functools import wraps
 def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
+        print("----------entre?")
         user = session.get('profile', {})
         # You would add a check here and usethe user id or something to fetch
         # the other data for that user/check if they exist
         if user:
+            print("---------usuario encontrado")
             return f(*args, **kwargs)
+        print("----------usuario no encontrado, rediriguendo a login")
         return render_template("/login")#'You aint logged in, no page for u!'
     return decorated_function

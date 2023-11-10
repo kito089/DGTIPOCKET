2e3a73268fc92e3be9546fe70e39dad374846f98
kito089 <21301061550046@cetis155.edu.mx>
2023-11-10 10:43:17 -0600

    apekaaaaaaaaaaaaaaaaa2

--- AndroidWeb/app/src/main/java/com/web4edu/edulifepocketweb/MainActivity.java
+++ AndroidWeb/app/src/main/java/com/web4edu/edulifepocketweb/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         myWebView = findViewById(R.id.web1);
         myWebSettings = myWebView.getSettings();
+        myWebSettings.setUserAgentString("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3");
         myWebSettings.setJavaScriptEnabled(true);
         myWebSettings.setDomStorageEnabled(true);
         myWebView.loadUrl("https://patotipo.pythonanywhere.com/");

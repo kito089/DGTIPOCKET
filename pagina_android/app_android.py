dc82923aad0b2c4577ba1f25ae14aa376e25ffa7
kito089 <21301061550046@cetis155.edu.mx>
2023-11-14 11:24:18 -0600

    XD

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -42,11 +42,11 @@ google = oauth.register(
     #token_response=None,
     api_base_url='https://www.googleapis.com/oauth2/v1/',
     #--userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    #server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
-    #client_kwargs={'scope': 'openid email profile'}
+    server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
+    client_kwargs={'scope': 'openid email profile'}
 
-    redirect_uri=lambda: url_for('auth', _external=True),
-    client_kwargs={'scope': 'https://www.googleapis.com/auth/calendar.readonly'},
+    #redirect_uri=lambda: url_for('auth', _external=True),
+    #client_kwargs={'scope': 'https://www.googleapis.com/auth/calendar.readonly'},
 )
 
 # Definir una ruta para la página principal

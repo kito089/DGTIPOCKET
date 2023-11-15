66845f61c89d3c5d46974af4bd3dfc1d9b3f8a58
kito089 <21301061550046@cetis155.edu.mx>
2023-11-15 12:25:20 -0600

    tratando de entender unu

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -4,12 +4,12 @@ from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from google.auth.transport.requests import Request
-import google.auth.transport.requests
+from googleapiclient.errors import HttpError
 import google.auth.exceptions
 import os
+import os.path
 from python.bd import *
-from datetime import timedelta
-
+import datetime
 
 # decorator for routes that should be accessible only by logged in users
 from python.funciones_auth import login_required
@@ -25,8 +25,6 @@ app = Flask(__name__)
 # XDDDDDDDD
 # Session config
 app.secret_key = os.getenv("APP_SECRET_KEY")
-#app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
-#app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
 
 oauth = OAuth(app)
 
@@ -50,6 +48,8 @@ google = oauth.register(
     client_kwargs={'scope': 'openid email profile https://www.googleapis.com/auth/calendar'},
 )
 
+SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
+
 # Definir una ruta para la página principal
 @app.route('/')
 @login_required
@@ -60,6 +60,55 @@ def index():
     parametros = dict(session)['profile']
     print("sesion")
     print(dict(session))
+
+    # creds = None
+    # # The file token.json stores the user's access and refresh tokens, and is
+    # # created automatically when the authorization flow completes for the first
+    # # time.
+    # if os.path.exists("token.json"):
+    #     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # # If there are no (valid) credentials available, let the user log in.
+    # if not creds or not creds.valid:
+    #     if creds and creds.expired and creds.refresh_token:
+    #         creds.refresh(Request())
+    #     else:
+    #         flow = InstalledAppFlow.from_client_secrets_file(
+    #             "credentials.json", SCOPES
+    #         )
+    #         creds = flow.run_local_server(port=0)
+    #     # Save the credentials for the next run
+    #     with open("token.json", "w") as token:
+    #         token.write(creds.to_json())
+
+    # try:
+    #     service = build("calendar", "v3", credentials=creds)
+
+    #     # Call the Calendar API
+    #     now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+    #     print("Getting the upcoming 10 events")
+    #     events_result = (
+    #         service.events()
+    #         .list(
+    #             calendarId="primary",
+    #             timeMin=now,
+    #             maxResults=10,
+    #             singleEvents=True,
+    #             orderBy="startTime",
+    #         )
+    #         .execute()
+    #     )
+    #     events = events_result.get("items", [])
+
+    #     if not events:
+    #         print("No upcoming events found.")
+
+    #     # Prints the start and name of the next 10 events
+    #     for event in events:
+    #         start = event["start"].get("dateTime", event["start"].get("date"))
+    #         print(start, event["summary"])
+
+    # except HttpError as error:
+    #     print(f"An error occurred: {error}")
 
     return render_template('indexapp.html', parametros = parametros,noticias=noticias)
 
@@ -86,6 +135,8 @@ def authorize():
     print(user)
     print("---------------token")
     print(token)
+    with open("token.json", "w") as tok:
+        tok.write(token.to_json())
     print("---------------user_info")
     print(user_info)
     #print(info)

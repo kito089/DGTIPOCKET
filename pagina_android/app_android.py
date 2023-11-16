b1aa9447d2cad6a37a69986cb93396e282eace10
kito089 <21301061550046@cetis155.edu.mx>
2023-11-15 23:26:28 -0600

    toks (/≧▽≦)/

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -133,7 +133,7 @@ def authorize():
     user_info = resp.json()
     user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
     session['profile'] = user_info
-    session['tok_info'] = {'client_id':os.getenv("GOOGLE_CLIENT_ID"),'client_secret': os.getenv("GOOGLE_CLIENT_SECRET"),'refresh_token':token.get('refresh_token')}
+    session['tok_info'] = {'client_id':os.getenv("GOOGLE_CLIENT_ID"),'client_secret': os.getenv("GOOGLE_CLIENT_SECRET"),'refresh_token':token.get('refresh_token'), 'access_token':token.get('access_token')}
     #session['user_info'] = info
     print("--------------datos")
     print("--------------resp")

6c79552d0a0fd91bcc0af224cf90668e9b1ccc86
kito089 <21301061550046@cetis155.edu.mx>
2023-11-15 23:08:55 -0600

    waos 👌(*^▽^*)

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -59,57 +59,58 @@ def index():
     noticias = bd.obtenerTablas("noticias")
     bd.exit()
     parametros = dict(session)['profile']
+    toks = dict(session)['tok_info']
     print("sesion")
     print(dict(session))
+    print("session token")
+    print(toks)
 
-    # creds = None
-    # # The file token.json stores the user's access and refresh tokens, and is
-    # # created automatically when the authorization flow completes for the first
-    # # time.
-    # if os.path.exists("token.json"):
-    #     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # # If there are no (valid) credentials available, let the user log in.
-    # if not creds or not creds.valid:
-    #     if creds and creds.expired and creds.refresh_token:
-    #         creds.refresh(Request())
-    #     else:
-    #         flow = InstalledAppFlow.from_client_secrets_file(
-    #             "credentials.json", SCOPES
-    #         )
-    #         creds = flow.run_local_server(port=0)
-    #     # Save the credentials for the next run
-    #     with open("token.json", "w") as token:
-    #         token.write(creds.to_json())
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    creds = Credentials.from_authorized_user_file(toks, SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        print("token no valido por alguna razon :v")
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            # flow = InstalledAppFlow.from_client_secrets_file(
+            #     "credentials.json", SCOPES
+            # )
+            # creds = flow.run_local_server(port=0)
+            print("no hay un refresh token")        
 
-    # try:
-    #     service = build("calendar", "v3", credentials=creds)
+    try:
+        service = build("calendar", "v3", credentials=creds)
 
-    #     # Call the Calendar API
-    #     now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
-    #     print("Getting the upcoming 10 events")
-    #     events_result = (
-    #         service.events()
-    #         .list(
-    #             calendarId="primary",
-    #             timeMin=now,
-    #             maxResults=10,
-    #             singleEvents=True,
-    #             orderBy="startTime",
-    #         )
-    #         .execute()
-    #     )
-    #     events = events_result.get("items", [])
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the upcoming 10 events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=10,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
 
-    #     if not events:
-    #         print("No upcoming events found.")
+        if not events:
+            print("No upcoming events found.")
 
-    #     # Prints the start and name of the next 10 events
-    #     for event in events:
-    #         start = event["start"].get("dateTime", event["start"].get("date"))
-    #         print(start, event["summary"])
+        # Prints the start and name of the next 10 events
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            print(start, event["summary"])
 
-    # except HttpError as error:
-    #     print(f"An error occurred: {error}")
+    except HttpError as error:
+        print(f"An error occurred: {error}")
 
     return render_template('indexapp.html', parametros = parametros,noticias=noticias)
 
@@ -131,7 +132,7 @@ def authorize():
     resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
     user_info = resp.json()
     user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
-    
+
     session['profile'] = user_info
     del token['userinfo']
     session['tok_info'] = token

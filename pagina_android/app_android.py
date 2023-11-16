b590e3d25fc8bdcbf71e55d71225465c9475713f
kito089 <21301061550046@cetis155.edu.mx>
2023-11-15 23:57:41 -0600

    token uri ＞﹏＜

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -128,7 +128,7 @@ def login():
 def authorize():
     google = oauth.create_client('google')  # create the google oauth client
     token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    tokens = {'client_id':os.getenv("GOOGLE_CLIENT_ID"),'client_secret': os.getenv("GOOGLE_CLIENT_SECRET"),'refresh_token':token.get('refresh_token'), 'access_token':token.get('access_token')}
+    tokens = {'client_id':os.getenv("GOOGLE_CLIENT_ID"),'client_secret': os.getenv("GOOGLE_CLIENT_SECRET"),'refresh_token':token.get('refresh_token'), 'access_token':token.get('access_token'), 'token_uri': 'https://accounts.google.com/o/oauth2/token'}
     #token_dict = token.as_dict()
     #token_json = json.dumps(token_dict, indent=2)
 
@@ -139,8 +139,8 @@ def authorize():
     session['tok_info'] = tokens
     print("---------------toks")
     print(tokens)
-    with open("token.txt", "w") as tok:
-        tok.write(str(token))
+    # with open("token.txt", "w") as tok:
+    #     tok.write(str(token))
     print("fin")
     #session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
     return redirect('/')

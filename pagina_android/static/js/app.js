969aef1e1190b070e80be9a2e202e67db6ac466e
yael <21301061550024@cetis155.edu.mx>
2023-11-17 11:44:33 -0600

    carrusel noticias 1.2

--- pagina_android/static/js/app.js
+++ pagina_android/static/js/app.js
@@ -1,17 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
 	let index = 0;
-	const cards = document.querySelectorAll('.card');
+	const noticias = document.querySelectorAll('.noticia');
   
-	function mostrarSiguienteCard() {
-	  cards[index].classList.remove('active');
-	  cards[index].style.opacity = 0;
+	function mostrarSiguienteNoticia() {
+	  noticias[index].classList.remove('active');
+	  noticias[index].style.opacity = 0;
 	  
-	  index = (index + 1) % cards.length;
+	  index = (index + 1) % noticias.length;
 	  
-	  cards[index].classList.add('active');
-	  cards[index].style.opacity = 1;
+	  noticias[index].classList.add('active');
+	  noticias[index].style.opacity = 1;
 	}
   
-	setInterval(mostrarSiguienteCard, 3000); // Cambiar de tarjeta cada 3 segundos
+	setInterval(mostrarSiguienteNoticia, 2000); // Cambiar de noticia cada 3 segundos
   });
+  
   

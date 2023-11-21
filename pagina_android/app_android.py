9b12215a0d8a7654ba5ad45081eb20fd7002bf36
kito089 <21301061550046@cetis155.edu.mx>
2023-11-21 14:20:04 -0600

    solucion

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -263,8 +263,6 @@ def boleta():
 
     genboleta(datosC, datosG)
     archivo = os.path.expanduser('~/DGTIPOCKET/editar_word/'+nombr[0]+"_"+nombr[1]+'.docx')
-
-    word2pdf(os.path.expanduser('~/DGTIPOCKET/editar_word/'+nombr[0]+"_"+nombr[1]))
     
     return send_file(archivo, as_attachment=True)
     #ruta_docx = os.path.expanduser('~/DGTIPOCKET/editar_word/{0}.docx'.format(nombr[3][0]+"_"+nombr[3][1]))

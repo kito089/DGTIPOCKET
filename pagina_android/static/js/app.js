a79623fcbfd1e88ff6b6af93542197c9a81287f6
yael <21301061550024@cetis155.edu.mx>
2024-01-25 23:34:20 -0600

    pura verga arriba el css

--- pagina_android/static/js/app.js
+++ pagina_android/static/js/app.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
+	// Verifica si es un dispositivo móvil (ancho de pantalla <= 767px)
+	if (window.innerWidth <= 767) {
+	  return; // No activa la funcionalidad en dispositivos móviles
+	}
+  
 	const carousel = document.querySelector(".carousel");
 	const cards = document.querySelectorAll(".card");
-
+  
 	let currentIndex = 0;
-
+  
 	function showCard(index) {
-		const newPosition = -index * 100 + "%";
-		carousel.style.transform = "translateX(" + newPosition + ")";
+	  const newPosition = -index * 100 + "%";
+	  carousel.style.transform = "translateX(" + newPosition + ")";
 	}
-
+  
 	function nextCard() {
-		currentIndex = (currentIndex + 1) % cards.length;
-		showCard(currentIndex);
+	  currentIndex = (currentIndex + 1) % cards.length;
+	  showCard(currentIndex);
 	}
+  
+	setInterval(nextCard, 4000); // Cambia la tarjeta cada 4 segundos (ajusta según sea necesario)
+  });
 
-	setInterval(nextCard, 4000); // Cambia la tarjeta cada 3 segundos (ajusta según sea necesario)
-
-
-});
-
+  
+  
 document.addEventListener("DOMContentLoaded", function () {
 	
 	const carouselA = document.querySelector(".carouselA");

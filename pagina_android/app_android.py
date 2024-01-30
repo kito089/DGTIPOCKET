b27b3015a6bb941baba21ae16bc90f91aeb033e3
kito089 <21301061550046@cetis155.edu.mx>
2024-01-30 09:18:15 -0600

    le tengo fe

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -622,6 +622,8 @@ def leerE(nombre, time):
                                 datos_celda[17],datos_celda[24],str(idm),str(al)]
                         pre = db.seleccion("evaluacion_e","idevaluacion_e","periodo = '{}' and acreditacion = '{}' and materias_idmaterias = '{}' and alumnos_idalumnos = '{}'".format(datos_celda[17],datos_celda[24],idm,al))
                         if len(pre) > 0:
+                            datos = [pre[0][0],datos_celda[13],datos_celda[14],datos_celda[15],datos_celda[20],datos_celda[21],datos_celda[22],
+                                datos_celda[17],datos_celda[24],str(idm),str(al)]
                             db.actualizarRegistro("evaluacion_e",str(pre[0][0]),datos)
                         else:
                             db.insertarRegistro("evaluacion_e",datos)
@@ -683,6 +685,8 @@ def leerTC(nombre, time):
                                     datos_celda[17],datos_celda[24],str(idm),str(al)]
                             pre = db.seleccion("evaluacion_tc","idevaluacion_tc","periodo = '{}' and acreditacion = '{}' and submodulos_idsubmodulos = '{}' and alumnos_idalumnos = '{}'".format(datos_celda[17],datos_celda[24],idm,al))
                             if len(pre) > 0:
+                                datos = [pre[0][0],datos_celda[13],datos_celda[14],datos_celda[15],datos_celda[20],datos_celda[21],datos_celda[22],
+                                    datos_celda[17],datos_celda[24],str(idm),str(al)]
                                 db.actualizarRegistro("evaluacion_tc",str(pre[0][0]),datos)
                             else:
                                 db.insertarRegistro("evaluacion_tc",datos)

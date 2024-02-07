c61c6c495a250c478372274d49c20c0590d35359
yael <21301061550024@cetis155.edu.mx>
2024-02-07 07:37:43 -0600

    tiki taka toko como quiera yo la toco

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -24,8 +24,8 @@ from googleapiclient.errors import HttpError
 from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 import io
 
-#project_folder = os.path.expanduser('~/DGTIPOCKET/pagina_android')
-project_folder = 'C:/Users/jezar/Downloads/DGTIPOCKET/pagina_android'
+project_folder = os.path.expanduser('~/DGTIPOCKET/pagina_android')
+#project_folder = 'C:/Users/jezar/Downloads/DGTIPOCKET/pagina_android'
 load_dotenv(os.path.join(project_folder, '.env'))
 
 app = Flask(__name__)

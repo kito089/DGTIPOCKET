7aebd016ae636201de410591dbd0c1cafbf40ad6
kito089 <21301061550046@cetis155.edu.mx>
2024-02-15 07:43:21 -0600

    sdfsdfsf

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -883,8 +883,6 @@ def driveMas():
             for l in letras:
                 for i in range(6):
                     check = request.form.get(str(i+1)+str(l[0]))
-                    print("grupo y grado: ",l,i)
-                    print(check)
                     if check == 'True':
                         bd.insertarRegistroConID("cuadernillos_has_grupo",[str(idc),str(i+1),str(l[0])])
             

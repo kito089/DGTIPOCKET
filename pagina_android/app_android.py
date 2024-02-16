45ea5c8f22abf4c942c127a9607955987f833979
yael <21301061550024@cetis155.edu.mx>
2024-02-15 23:26:51 -0600

    xd valio verga

--- pagina_android/app_android.py
+++ pagina_android/app_android.py
@@ -233,7 +233,7 @@ def obtener_fechaD(anio, mes, dia):
     "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
     "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
     eventos=obtenerEventos()
-    fecha_predeterminada = anio+"-"+mes+"-"+dia
+    fecha_predeterminada = str(anio+"-"+mes+"-"+dia)
     print(fecha_predeterminada)
     # Filtrar las entradas con la fecha predeterminada
     actuales = [evento for evento in eventos if 'start' in evento and evento['start'].startswith(fecha_predeterminada)]
